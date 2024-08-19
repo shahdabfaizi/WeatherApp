e4c048924c54167038bfb46f48d4ebf0d539220c
shahdabfaizi <shadabfaizi202@gmail.com>
2024-08-19 20:42:18 +0200

    styling & java

--- src/api.js
+++ src/api.js
@@ -3,40 +3,71 @@ export async function fetchWeatherData() {
   const city = "Wesendorf";
   const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3&lang=de`;
 
-  const response = await fetch(url);
-  const data = await response.json();
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-  const temperature = data.current.temp_c;
-  const condition = data.current.condition.text;
-  const cityName = data.location.name;
-  const iconUrl = data.current.condition.icon;
+    const temperature = data.current.temp_c;
+    const condition = data.current.condition.text;
+    const cityName = data.location.name;
+    const iconUrl = data.current.condition.icon;
 
-  document.getElementById("temperature").innerText = `${temperature}°C`;
-  document.getElementById("infoNew").innerText = condition;
-  document.getElementById("CityName").innerText = cityName;
-  document.getElementById(
-    "img"
-  ).innerHTML = `<img src="https:${iconUrl}" alt="${condition}" />`;
-
-  const forecastDays = data.forecast.forecastday;
-
-  forecastDays.forEach((day, index) => {
-    const date = day.date;
-    const maxTemp = day.day.maxtemp_c;
-    const minTemp = day.day.mintemp_c;
-    const conditionText = day.day.condition.text;
-    const conditionIcon = day.day.condition.icon;
-
-    document.getElementById(`day${index + 1}-date`).innerText = date;
+    document.getElementById("temperature").innerText = `${temperature}°C`;
+    document.getElementById("infoNew").innerText = condition;
+    document.getElementById("CityName").innerText = cityName;
     document.getElementById(
-      `day${index + 1}-temp`
-    ).innerText = `${maxTemp}°C / ${minTemp}°C`;
-    document.getElementById(`day${index + 1}-condition`).innerText =
-      conditionText;
-    document.getElementById(
-      `day${index + 1}-icon`
-    ).innerHTML = `<img src="https:${conditionIcon}" alt="${conditionText}" />`;
-  });
+      "img"
+    ).innerHTML = `<img src="https:${iconUrl}" alt="${condition}" />`;
+
+    const forecastDays = data.forecast.forecastday;
+
+    forecastDays.forEach((day, index) => {
+      const date = day.date;
+      const maxTemp = day.day.maxtemp_c;
+      const minTemp = day.day.mintemp_c;
+      const conditionText = day.day.condition.text;
+      const conditionIcon = day.day.condition.icon;
+      const days = document.createElement("div");
+      days.classList.add("dayBox");
+
+      document.getElementById(`day${index + 1}-date`).innerText = date;
+      document.getElementById(
+        `day${index + 1}-temp`
+      ).innerText = `${maxTemp}°C / ${minTemp}°C`;
+      document.getElementById(`day${index + 1}-condition`).innerText =
+        conditionText;
+      document.getElementById(
+        `day${index + 1}-icon`
+      ).innerHTML = `<img src="https:${conditionIcon}" alt="${conditionText}" />`;
+    });
+
+    const hourlyData = forecastDays[0].hour;
+    const forecastContainer = document.getElementById("forecast");
+
+    hourlyData.forEach((hourData) => {
+      const hourBlock = document.createElement("div");
+      hourBlock.classList.add("timeBox");
+
+      const hourTime =
+        new Date(hourData.time).getHours().toString().padStart(2, "0") + ":00";
+      const hourTemp = `${hourData.temp_c}°C`;
+      const hourCondition = hourData.condition.text;
+      const hourIconUrl = hourData.condition.icon;
+
+      hourBlock.innerHTML = `
+      <div>
+        <h3 class="hour-time">${hourTime}</h3>
+        <div class="hour-icon">
+          <img src="https:${hourIconUrl}" alt="${hourCondition}" />
+        </div>
+        <p class="hour-temp">${hourTemp}</p>
+        <p class="hour-condition">${hourCondition}</p>
+      </div>`;
+      forecastContainer.appendChild(hourBlock);
+    });
+  } catch (error) {
+    console.error("Fehler beim Abrufen der Wetterdaten:", error);
+  }
 }
 
 fetchWeatherData();
